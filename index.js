bdf5652f1b5d2368f9fa56ee14c6f245e336699d
jordantyeburn <jordank@kilburnindustries.com>
2022-02-10 11:58:45 -0600

    remove secret key and add Rapid API verification

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import express from "express";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: "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", //process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 const app = express();
 
 app.get("/api/emoji", async (req, res) => {
+  if (
+    !req.headers.supersecretheader ||
+    req.headers.supersecretheader !== process.env.SUPER_SECRET_HEADER
+  )
+    return res.status(401).send("You don't know the password!");
+
   const { input } = req.query;
   if (!input) return res.status(400).send("You need to send an input!");
 
